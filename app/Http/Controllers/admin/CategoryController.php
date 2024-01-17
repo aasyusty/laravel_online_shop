c0b59841580feeba647a5df0f8f2f6d5116db5ed
aasyusty <abrahamtde@gmail.com>
2024-01-17 12:24:58 -0400

    primer repositorio

---
+++ app/Http/Controllers/admin/CategoryController.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Category;
+use App\Models\TempImage;
+use Illuminate\Support\Facades\File;
+
+class CategoryController extends Controller
+{
+    public function index(Request $request){
+        $categories = Category::latest();
+
+        if (!empty($request->get('keyword'))){
+            $categories = $categories->where('name', 'like', '%'.$request->get('keyword').'%');
+        }
+
+        $categories = $categories->paginate(10); // Corrección aquí
+        return view('admin.category.list', compact('categories'));
+    }
+
+    public function create(){
+        return view('admin.category.create');
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'slug' => 'required|unique:categories',
+        ]);
+
+        if ($validator->passes()) {
+
+            $category = new Category();
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save();
+
+            //Save Image Here
+
+            if(!empty($request->image_id)){
+                $tempImage = TempImage::find($request->image_id);
+                $extArray = explode('.',$tempImage->name);
+                $ext = last($extArray);
+
+                $newImageName = $category->id.'.'.$ext;
+                $sPath = public_path().'/temp/'.$tempImage->name;
+                $dPath = public_path().'/uploads/categories/'.$tempImage->name;
+                File::copy($sPath,$dPath);
+
+                //generate image thumbail
+                $dPath = public_path().'/uploads/category/thumb/'.$newImageName;
+                $img = image::make($sPath);
+                //$img->resize(450,600);
+                $img->fit(450, 600, function ($constraint) {
+                    $constraint->upsize();
+                });
+                $img->save($dPath);
+
+                $category->image = $newImageName;
+                $category->save();
+
+            }
+
+            $request->session()->flash('success', 'Categoria agregada satisfactoriamente');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Categoria agregada satisfactoriamente'
+                
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+                
+            ]);
+        }
+    }
+
+    public function edit($categoryId, Request $request) {
+        
+        // echo $categoryId;
+        $category = category::find($categoryId);
+        if (empty($category)) {
+            return redirect()->route('categories.index');
+        }
+                
+        return view('admin.category.edit',compact('category'));
+    }
+
+    public function update($categoryId, Request $request){
+        
+        $category = category::find($categoryId);
+        
+        if (empty($category)) {
+            $request->session()->flash('error', 'Categoria no encontrada');
+            return response()->json([
+                'status' => false,
+                'notFound' => true,
+                'message' => 'category not found'
+                ]);
+        }
+        
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug,'.$category->id.',id',
+        ]);
+
+        if ($validator->passes()) {
+
+            //$category = new Category();
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save();
+
+            $oldImage = $category->image;
+
+            //Save Image Here
+            if(!empty($request->image_id)) {
+                $tempImage = TempImage::find($request->image_id);
+                $extArray = explode('.',$tempImage->name);
+                $ext = last($extArray);
+
+                $newImageName = $category->id.'-'.time().'.'.$ext;
+                $sPath = public_path().'/temp/'.$tempImage->name;
+                $dPath = public_path().'/uploads/categories/'.$newImageName;
+                File::copy($sPath,$dPath);
+
+
+                //generate image thumbail
+                $dPath = public_path().'/uploads/category/thumb/'.$newImageName;
+                $img = image::make($sPath);
+                //$img->resize(450,600);
+                $img->fit(450, 600, function ($constraint) {
+                    $constraint->upsize();
+                });
+                $img->save($dPath);
+
+                $category->image = $newImageName;
+                $category->save();
+
+                // delete old images here
+                file::delete(public_path().'/uploads/category/thumb/'.$oldImage);
+                file::delete(public_path().'/uploads/category/'.$oldImage);
+
+
+            }
+
+            $request->session()->flash('success', 'Categoria modificada satisfactoriamente');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Categoria modificada satisfactoriamente'
+                
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+                
+            ]);
+        }
+    }
+
+    public function destroy($categoryId, Request $request){
+        $category = category::find($categoryId);
+        if(empty($category)) {
+            $request->session()->flash('error','Category not found');
+            return redirect()->route('categories.index');
+        }
+
+        file::delete(public_path().'/uploads/category/thumb/'.$category->image);
+        file::delete(public_path().'/uploads/category/'.$category->image);
+
+        $category->delete();
+
+        $request->session()->flash('success','Category deleted successfully');
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Category deleted successfully'
+            
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\HomeController;
+use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\subcategoryController;
+use App\Http\Controllers\admin\TempImagesController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\FrontController;
+
+
+/*Route::get('/', function () {
+//    return view('welcome');
+});*/
+
+//Route::get('/',[FrontController::class, 'index'])->name('front.home');
+Route::get('/',[FrontController::class, 'index'])->name('front.home');
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::group(['middleware' => 'admin.guest'], function () {
+        Route::get('/login', [AdminLoginController::class, 'index'])->name('admin.login');
+        Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+    });
+
+    Route::group(['middleware' => 'admin.auth'], function () {
+        Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+        Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
+
+        // Category routes
+        Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+        Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+        Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+        Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+        Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+        Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.delete');
+
+
+        // sub category routes
+        Route::get('/sub-categories', [subcategoryController::class, 'index'])->name('sub-categories.index');
+        Route::get('/sub-categories/create', [subcategoryController::class, 'create'])->name('sub-categories.create');
+        Route::post('/sub-categories', [subcategoryController::class, 'store'])->name('sub-categories.store');
+
+        //temp-images.create
+        Route::post('/upload-temp-image', [TempImagesController::class, 'create'])->name('temp-images.create');
+
+
+
+        Route::get('/getSlug',function(Request $request){
+            $slug = '';
+            if (!empty($request->title)) {
+                $slug = Str::slug($request->title);
+            }
+            return response()->json([
+                'status' => true,
+                'slug' => $slug
+            ]);
+
+
+        })->name('getSlug');
+
+
+    });
+});
+
